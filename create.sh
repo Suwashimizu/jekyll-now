33ce75eeb254ffd1b6d2a696fcaa70a3725fbbe1
suwa <suwawa2013@gmail.com>
2017-03-11 23:07:36 +0900

    createコマンド作成

---
+++ create.sh
@@ -0,0 +1,14 @@
+# /bin/sh
+
+echo Titleを入力してください
+# キー入力待ち
+read title
+
+# 空文字チェック,空文字でなければtrue
+if [ -n "$title" ]; then
+   # 日付を変数に格納
+   to_day=`date +"%Y-%m-%d"`
+   echo $to_day-$title.mdを作成しました
+   # fileの作成
+   touch _posts/$to_day-$title.md
+fi
